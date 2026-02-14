bd4d34b34f0f0b1fa887adba26d3c1716fec2b10
Frank <172104042+Frank-678@users.noreply.github.com>
2026-02-14 13:22:58 +0000

    Sat, Feb 14, 2026, 9:22 PM +08:00

--- Assignment/assign-2-adts/1-word-ladder/src/word-ladder.cpp
+++ Assignment/assign-2-adts/1-word-ladder/src/word-ladder.cpp
@@ -25,6 +25,37 @@ static string getWord(const Lexicon& english, const string& prompt) {
 
 static void generateLadder(const Lexicon& english, const string& start, const string& end) {
     cout << "Here's where you'll search for a word ladder connecting \"" << start << "\" to \"" << end << "\"." << endl;
+    int l = start.length();
+    Vector<string> ladder {start};
+    Queue<Vector<string> > q {ladder};
+    Set<string> visited {start};
+    while (!q.isEmpty()) {
+        Vector<string> wordladder = q.dequeue();
+        string word = wordladder[wordladder.size()-1];
+        if (word == end) {
+            cout << "Found ladder:";
+            for (string s : wordladder) {
+                cout << " " << s;
+            }
+            cout << endl << endl;
+            return;
+        }
+        for (int i = 0; i < l; i++) {
+            for (char c = 'a'; c <= 'z'; c++) {
+                string tempword = word;
+                tempword[i] = c;
+                if (english.contains(tempword) && !visited.contains(tempword)) {
+                    Vector<string> newladder = wordladder;
+                    newladder.add(tempword);
+                    q.enqueue(newladder);
+                    visited.add(tempword);
+                }
+            }
+        }
+    }
+
+    // 该单词没有邻近的单词，queue为空
+    cout << "No word ladder found." << endl;
 }
 
 static const string kEnglishLanguageDatafile = "res/dictionary.txt";
@@ -35,7 +66,11 @@ static void playWordLadder() {
         if (start.empty()) break;
         string end = getWord(english, "Please enter the destination word [return to quit]: ");
         if (end.empty()) break;
-        generateLadder(english, start, end);
+        if (start.length() != end.length()) {
+            cout << "The two endpoints must contain the same number of characters, or else no word ladder can exist." << endl;
+        } else {
+            generateLadder(english, start, end);
+        }
     }
 }
 
--- Assignment/assign-2-adts/2-maze-generator/src/maze-generator.cpp
+++ Assignment/assign-2-adts/2-maze-generator/src/maze-generator.cpp
@@ -5,10 +5,16 @@
  */
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 #include "console.h"
 #include "simpio.h"
+#include "set.h"
+
+#include "maze-graphics.h"
+#include "maze-types.h"
+
 
 static int getMazeDimension(string prompt,
                             int minDimension = 7, int maxDimension = 50) {
@@ -22,12 +28,60 @@ static int getMazeDimension(string prompt,
     }
 }
 
+void initInternal(int dimention, Set<cell>& cells, Set<wall>& walls) {
+    for (int i = 0; i < dimention; ++i) {
+        for (int j = 0; j < dimentionl ++j) {
+            cell c {i, j};
+            cells.add(c);
+        }
+    }
+
+    for (const cell& c1 : cells) {
+        for (const cell& c2 : cells) {
+            if (c1.row == c2.row && c2.col - c1.col == 1
+                || c1.col == c2.col && c2.row - c1.row == 1) {
+                wall w {c1, c2};  // c1左/上于c2
+                walls.add(w);
+            }
+        }
+    }
+}
+
+void randomRemove(int dimention, Set<cell>& cells, Set<wall>& walls) {
+    for (const cell& c1 : cells) {
+        for (const cell& c2 : cells) {
+            if (!walls.contain({c1, c2})) {
+                Set<cell> chamber {c1, c2};
+            }
+
+
+        }
+    }
+}
+
+void render(int dimention, MazeGeneratorView& display){
+    display.MazeGeneratorView();
+    display.setDimension(dimention);
+    display.drawBorder();
+}
+
 int main() {
     while (true) {
         int dimension = getMazeDimension("What should the dimension of your maze be [0 to exit]? ");
         if (dimension == 0) break;
         cout << "This is where I'd animate the construction of a maze of dimension " << dimension << "." << endl;
+
+        Set<cell> cells;
+        Set<wall> walls;
+        initInternal(dimension, cells, walls);
+
+        randomRemove(dimension, cells, walls);
+
+        MazeGeneratorView display;
+        render(dimension, display);
+        display.repaint();
     }
 
     return 0;
 }
+
