5053451803db8a8d8f860fa79812c057c7fa40e9
Frank <172104042+Frank-678@users.noreply.github.com>
2026-02-15 03:08:00 +0000

    Sun, Feb 15, 2026, 11:08 AM +08:00

--- Assignment/assign-2-adts/3-random-sentence-generator/src/random-sentence-generator.cpp
+++ Assignment/assign-2-adts/3-random-sentence-generator/src/random-sentence-generator.cpp
@@ -8,11 +8,15 @@
 
 #include <iostream>
 #include <fstream>
+#include <random>
+#include <string>
 using namespace std;
 
 #include "console.h"
+#include "map.h"
 #include "simpio.h"   // for getLine
 #include "strlib.h"   // for toLowerCase, trim
+#include "vector.h"
 
 static const string kGrammarsDirectory = "res/grammars/";
 static const string kGrammarFileExtension = ".g";
@@ -37,14 +41,73 @@ static string getFileName() {
     }
 }
 
+void readGrammar(ifstream& input, Map<string, Vector<string> >& grammar);
+void replaceToken(Map<string, Vector<string> >& grammar);
+void printSentence(Map<string, Vector<string> >& grammar);
+
 int main() {
     while (true) {
         string filename = getFileName();
         if (filename.empty()) break;
         cout << "Here's where you read in the \"" << filename << "\" grammar "
              << "and generate three random sentences." << endl;
+
+        ifstream input;
+        input.open(filename.c_str());
+        Map<string, Vector<string> > grammar;
+        readGrammar(input, grammar);
+        input.close();
+
+        replaceToken(grammar);
+        printSentence(grammar);
     }
     
     cout << "Thanks for playing!" << endl;
     return 0;
+}
+
+void readGrammar(ifstream& input, Map<string, Vector<string> >& grammar) {
+    while (true) {
+        string nonTerminal;
+        if (!getline(input, nonTerminal)) break;
+        if (trim(nonTerminal).empty()) continue;
+
+        string countStr;
+        getline(input, countStr);
+        int count = stringToInteger(countStr);
+
+        Vector<string> rules;
+        for (int i = 0; i < count; i++) {
+            string rule;
+            getline(input, rule);
+            rules.add(rule);
+        }
+
+        grammar[nonTerminal] = rules;
+    }
+}
+
+bool hasToken(Vector<string> start) {
+    for (string& sentence : start) {
+        if (sentence.find('<') != string::npos) return true;
+    }
+    return false;
+}
+
+void replaceToken(Map<string, Vector<string> >& grammar) {
+    while (hasToken(grammar["<start>"])) {
+        for (string& sentence : grammar["<start>"]) {
+            int start = sentence.find('<');
+            int end = sentence.find('>');
+            string token = sentence.substr(start, end - start + 1);
+            sentence = sentence.substr(0, start) + grammar[token][randomInteger(0, grammar[token].size() - 1)]
+                       + sentence.substr(end + 1, sentence.length() - end);
+        }
+    }
+}
+
+void printSentence(Map<string, Vector<string> >& grammar) {
+    for (string sentence : grammar["<start>"]) {
+        cout << sentence << endl;
+    }
 }
