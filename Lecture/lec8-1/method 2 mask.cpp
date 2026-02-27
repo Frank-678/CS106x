431bdeb7ad789c249d8b000495448f6662d7e4a2
Frank <172104042+Frank-678@users.noreply.github.com>
2026-02-27 12:37:34 +0000

    Fri, Feb 27, 2026, 8:37 PM +08:00

---
+++ Lecture/lec8-1/method 2 mask.cpp
@@ -0,0 +1,17 @@
+// 方法二：使用位遮挡 & 1<<x 便可以从左往右判断每一位
+#include <iostream>
+using namespace std;
+
+void printBinary(int n) {
+    if (n < 0) {
+        cout << '-';
+        n *= -1;
+    }
+
+    unsigned int un = (unsigned int)n;
+
+    unsigned int mask = 1;
+    while ((mask << 1) <= un) mask <<= 1;  // 先位移后判断！（如果n小于1<<x（x+1位）那么必然只有x位）
+
+    for (; mask; mask >>= 1) cout << ((un & mask) ? 1 : 0);
+}
