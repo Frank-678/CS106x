62b1e265adcbdab54e71687d59ab1f0ad512c0b6
Frank <172104042+Frank-678@users.noreply.github.com>
2026-02-27 11:33:35 +0000

    Fri, Feb 27, 2026, 7:33 PM +08:00

--- Lecture/lec8-1/Recursion-printBinary.cpp
+++ Lecture/lec8-1/Recursion-printBinary.cpp
@@ -1,5 +1,23 @@
 void printBinary(int n) {
-    
+    if (n == 0) {
+        cout << 0;
+    } else if (n == 1) {
+        cout << 1;
+    } else {
+        int exp1;
+        for (exp1 = 0; n > 1 << exp1; exp1++);
+        int rest = n - (1 << exp1 - 1);
+        int exp2;
+        for (exp2 = 0; rest > 1 << exp2; exp2++);
+
+        cout << 1;
+        for (int i = 0; i < exp2 - exp1 +3; i++) {
+            cout << 0;
+        }
+
+        printBinary(rest);
+    }
+
 }
 
 
