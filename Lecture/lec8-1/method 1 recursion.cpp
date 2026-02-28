225dc1f3e4f1497829f8f0c67e891ea95f2f717a
Frank <172104042+Frank-678@users.noreply.github.com>
2026-02-28 12:02:16 +0000

    Sat, Feb 28, 2026, 8:02 PM +08:00

--- Lecture/lec8-1/method 1 recursion.cpp
+++ Lecture/lec8-1/method 1 recursion.cpp
@@ -1,4 +1,9 @@
 // 这确实是一个看似基础却体现思路的重要性的题！Tricky！
+#include <iostream>
+#include "console.h"
+#include "simpio.h"
+#include "timer.h"
+using namespace std;
 
 void printBinary(int n) {
     if (n < 0) {
@@ -14,5 +19,19 @@ void printBinary(int n) {
     }
 }
 
+int main() {
+    while (true) {
+        int number = getInteger("Decimal number: ");
 
+        for (int var = 0; var < 10; ++var) {
+            Timer tim;
+            tim.start();
+            printBinary(number);
+            cout << endl;
+            tim.stop();
+            cout << "The code took " << tim.elapsed() << "ms to finish." << endl;
+        }
+    }
+    return 0;
+}
 
--- Lecture/lec8-4/fibonacci.cpp
+++ Lecture/lec8-4/fibonacci.cpp
@@ -1,7 +1,21 @@
 int fib(int n) {
     // base case
-    if (n == 1 || n == 2) return 1;
+    if (n == 0) return 0;
+    if (n == 1) return 1;
 
     // recursive case
     return fib(n - 1) + fib(n - 2);
 }
+
+
+// 通过https://www.codestepbystep.com/r/problem/view/cpp/loops/fibonacci测试
+// void fibonacci(int max) {
+//     cout << "Fibonacci sequence up to "<< max << ":" << endl;
+
+//     for (int i = 0; ; i++) {
+//         int n = fib(i);
+
+//         if (n >= max) break;
+//         cout << n << endl;
+//     }
+// }
