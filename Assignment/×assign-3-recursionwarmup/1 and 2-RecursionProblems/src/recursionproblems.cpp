26c15d5d2964c2b811bae31e091aa6ca1e76a5d9
Frank <172104042+Frank-678@users.noreply.github.com>
2026-03-03 05:50:07 +0000

    Tue, Mar 3, 2026, 1:50 PM +08:00

---
+++ Assignment/×assign-3-recursionwarmup/1 and 2-RecursionProblems/src/recursionproblems.cpp
@@ -0,0 +1,37 @@
+// This is the .cpp file you will edit and turn in.
+// We have provided a skeleton for you,
+// but you must finish it as described in the spec.
+// Also remove these comments here and add your own.
+// TODO: remove this comment header
+
+// Please feel free to add any other #includes you need!
+#include "recursionproblems.h"
+#include <cmath>
+#include <iostream>
+#include "hashmap.h"
+#include "map.h"
+#include "random.h"
+using namespace std;
+
+
+double weightOnKnees(int row, int col, const Vector<Vector<double> >& weights) {
+    // TODO: write this function
+    return 0.0;
+}
+
+double weightOnKneesFaster(int row, int col, const Vector<Vector<double> >& weights,
+                           Vector<Vector<double> >& memos) {
+    // TODO: write this function
+    return 0.0;
+}
+
+double weightOnKneesFaster(int row, int col, const Vector<Vector<double> >& weights) {
+    // TODO: write this function
+    return 0.0;
+}
+
+void drawSierpinskiTriangle(GWindow& gw, double x, double y, int size, int order) {
+    // TODO: write this function
+
+}
+
